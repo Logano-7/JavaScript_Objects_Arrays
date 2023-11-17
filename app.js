55b896079de0a7e4aa3821470148a2a059347638
Logan Martin <88mlogan@gmail.com>
2023-11-16 21:13:52 -0500

    complete

--- app.js
+++ app.js
@@ -1,9 +1,58 @@
-console.log("Hello World!\n==========\n");
+// Exercise 1
+const numbers = [2, 22, 12, 17, 18, 39, 129];
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+function arraySum(numbersSum) {
+  let sum = 0;
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+  for (let i = 0; i < numbersSum.length; i++)
+    // Iterating over the number array
+    sum += numbersSum[i];
+}
 
+// Exercise 2
+const book = {};
 
+book.title = "The Givng Tree";
+book.author = "Shel Silverstein";
+book.pages = 57;
+book.readCount = 100;
+
+book.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, read ${this.readCount} times.`;
+};
+console.log(book.info());
+
+// Exercise 3
+let sentence = "The quick brown fox jumps over the lazy dog";
+let wordsArray = sentence.split(" ");
+
+for (let i = 0; i < wordsArray.length; i++) {
+  let charactersArray = wordsArray[i].split("");
+  charactersArray.reverse();
+  wordsArray[i] = charactersArray.join("");
+}
+
+let reversedSentence = wordsArray.join(" ");
+
+console.log(reversedSentence);
+
+// Exercise 4
+
+let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
+let rows = csvData.split("\n");
+let headers = rows[0].split(",");
+let resultArray = [];
+
+for (let i = 1; i < rows.length; i++) {
+  let values = rows[i].split(",");
+
+  let record = {};
+
+  for (let j = 0; j < headers.length; j++) {
+    record[headers[j]] = values[j];
+  }
+
+  resultArray.push(record);
+}
+
+console.log(resultArray);
